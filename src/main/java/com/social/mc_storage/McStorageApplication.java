097c0c921d0fd57b4e0ae3a09c6a95738fe365cd
Daniil Tishin <danatiska5@gmail.com>
2024-08-03 17:04:03 +0300

    init project

---
+++ src/main/java/com/social/mc_storage/McStorageApplication.java
@@ -0,0 +1,13 @@
+package com.social.mc_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/mc_storage/McStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.mc_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
