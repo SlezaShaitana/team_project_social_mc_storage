b8b5f46b0deae8130c97cc52549443eb1d51a381
Daniil Tishin <danatiska5@gmail.com>
2024-08-20 12:11:21 +0300

    add Dockerfile and update yml

---
+++ src/main/java/com/social/mc_storage/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.social.mc_storage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/storage")
+                .allowedOrigins("http://79.174.80.200")
+                .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
