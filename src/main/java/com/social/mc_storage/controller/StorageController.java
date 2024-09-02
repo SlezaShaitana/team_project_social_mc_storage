276e5245e56319781fa01bbd19dfd22fb86300b4
GhostASA <artemsvist1809@gmail.com>
2024-09-02 22:21:09 +0300

    storage v2

--- src/main/java/com/social/mc_storage/controller/StorageController.java
+++ src/main/java/com/social/mc_storage/controller/StorageController.java
@@ -3,6 +3,7 @@ package com.social.mc_storage.controller;
 import com.social.mc_storage.dto.StorageDto;
 import com.social.mc_storage.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,12 +12,14 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/v1/storage")
 @RequiredArgsConstructor
+@Slf4j
 public class StorageController {
 
     private final S3Service service;
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(consumes = MediaType.IMAGE_JPEG_VALUE)
     public StorageDto uploadToStore(@RequestParam(required = false) MultipartFile file){
+        log.info("Controller file {}", file);
         return service.storage(file);
     }
 }
--- src/main/java/com/social/mc_storage/service/S3Service.java
+++ src/main/java/com/social/mc_storage/service/S3Service.java
@@ -13,52 +13,55 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import java.io.InputStream;
 import java.net.URI;
-    @Service
-    @Slf4j
-    public class S3Service {
 
-        private final String accessKey;
-        private final String secretKey;
-        private final String bucketName;
-        private final S3Client s3Client;
 
-        public S3Service(
-                @Value("${cloud.yandex.s3.access-key}") String accessKey,
-                @Value("${cloud.yandex.s3.secret-key}") String secretKey,
-                @Value("${cloud.yandex.s3.bucket-name}") String bucketName) {
+@Service
+@Slf4j
+public class S3Service {
 
-            this.accessKey = accessKey;
-            this.secretKey = secretKey;
-            this.bucketName = bucketName;
+    private final String accessKey;
+    private final String secretKey;
+    private final String bucketName;
+    private final S3Client s3Client;
 
-            this.s3Client = S3Client.builder()
-                    .endpointOverride(URI.create("https://storage.yandexcloud.net"))
-                    .region(Region.of("ru-central1"))
-                    .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(this.accessKey, this.secretKey)))
-                    .build();
-        }
+    public S3Service(
+            @Value("${cloud.yandex.s3.access-key}") String accessKey,
+            @Value("${cloud.yandex.s3.secret-key}") String secretKey,
+            @Value("${cloud.yandex.s3.bucket-name}") String bucketName) {
 
-        public StorageDto storage(MultipartFile file) {
-            String fileName = file.getOriginalFilename();
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.bucketName = bucketName;
 
-            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(fileName)
-                    .contentType(file.getContentType())
-                    .build();
-
-            try(InputStream inputStream = file.getInputStream()) {
-                long startTime = System.currentTimeMillis();
-                s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, file.getSize()));
-                long endTime = System.currentTimeMillis();
-                log.info("The image has been successfully sent to the storage in {} ms", (endTime - startTime));
-            } catch (Exception ex) {
-                log.error("The image was not sent to the storage, an error occurred: {}", ex.getMessage());
-                throw new RuntimeException("Failed to upload file to storage", ex);
-            }
-
-            StorageDto storageDto = new StorageDto();
-            storageDto.setFileName(String.format("https://%s.%s/%s", bucketName, "storage.yandexcloud.net", fileName));
-            return storageDto;
-        }
+        this.s3Client = S3Client.builder()
+                .endpointOverride(URI.create("https://storage.yandexcloud.net"))
+                .region(Region.of("ru-central1"))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(this.accessKey, this.secretKey)))
+                .build();
     }
+
+    public StorageDto storage(MultipartFile file) {
+        log.info("Uploading file {}", file.getOriginalFilename());
+        String fileName = file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
+
+        try(InputStream inputStream = file.getInputStream()) {
+            long startTime = System.currentTimeMillis();
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, file.getSize()));
+            long endTime = System.currentTimeMillis();
+            log.info("The image has been successfully sent to the storage in {} ms", (endTime - startTime));
+        } catch (Exception ex) {
+            log.error("The image was not sent to the storage, an error occurred: {}", ex.getMessage());
+            throw new RuntimeException("Failed to upload file to storage", ex);
+        }
+
+        StorageDto storageDto = new StorageDto();
+        storageDto.setFileName(String.format("https://%s.%s/%s", bucketName, "storage.yandexcloud.net", fileName));
+        return storageDto;
+    }
+}
