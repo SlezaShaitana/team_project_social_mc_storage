03fd52797367d012027c00f42ba9ef87aa861176
GhostASA <artemsvist1809@gmail.com>
2024-09-02 22:48:24 +0300

    storage v3

--- src/main/java/com/social/mc_storage/controller/StorageController.java
+++ src/main/java/com/social/mc_storage/controller/StorageController.java
@@ -4,7 +4,6 @@ import com.social.mc_storage.dto.StorageDto;
 import com.social.mc_storage.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,8 +16,8 @@ public class StorageController {
 
     private final S3Service service;
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public StorageDto uploadToStore(@RequestParam(required = false) MultipartFile file){
+    @PostMapping
+    public StorageDto uploadToStore(@RequestParam(name = "file", required = false) MultipartFile file){
         log.info("Controller file {}", file);
         return service.storage(file);
     }
